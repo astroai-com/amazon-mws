e19b0fe3f6da9926c53b7c90218c0d62b9acb59d
ad.chen <ad.chen@astroai.com>
2022-04-26 16:30:46 +0800

    up

--- src/MWSConfig.php
+++ src/MWSConfig.php
@@ -6,28 +6,34 @@ class MWSConfig {
      * Mws API市场配置
      */
     public static $MwsMarkets = [
-        'A2EUQ1WTGCTBG2' => 'mws.amazonservices.ca',
-        'A1MQXOICRS2Z7M' => 'mws.amazonservices.ca',
-        'ATVPDKIKX0DER' => 'mws.amazonservices.com',
-        'A2ZV50J4W1RKNI' => 'mws.amazonservices.com',
-        'A1AM78C64UM0Y8' => 'mws.amazonservices.com.mx',
-        'A1PA6795UKMFR9' => 'mws-eu.amazonservices.com',
+        // North America Region
+        'ATVPDKIKX0DER' => 'mws.amazonservices.com', // US
+        'A2ZV50J4W1RKNI' => 'mws.amazonservices.com', // US
+        'A2EUQ1WTGCTBG2' => 'mws.amazonservices.ca', // CA
+        'A1MQXOICRS2Z7M' => 'mws.amazonservices.ca', // CA
+        'A1AM78C64UM0Y8' => 'mws.amazonservices.com.mx', // MX
+        'A2Q3Y263D00KWC' => 'mws.amazonservices.com', // BR
+        // Europe Region
+        'A1PA6795UKMFR9' => 'mws-eu.amazonservices.com', // DE
         'A38D8NSA03LJTC' => 'mws-eu.amazonservices.com',
-        'A1RKKUPIHCS9HS' => 'mws-eu.amazonservices.com',
-        'A13V1IB3VIYZZH' => 'mws-eu.amazonservices.com',
+        'A13V1IB3VIYZZH' => 'mws-eu.amazonservices.com', // FR
         'A1ZFFQZ3HTUKT9' => 'mws-eu.amazonservices.com',
-        'A21TJRUUN4KGV' => 'mws.amazonservices.in',
-        'APJ6JRA9NG5V4' => 'mws-eu.amazonservices.com',
+        'A1RKKUPIHCS9HS' => 'mws-eu.amazonservices.com', // ES
+        'A21TJRUUN4KGV' => 'mws.amazonservices.in', // India
+        'APJ6JRA9NG5V4' => 'mws-eu.amazonservices.com', // Italy
         'A1F83G8C2ARO7P' => 'mws-eu.amazonservices.com', // UK
         'AZMDEXL2RVFNN' => 'mws-eu.amazonservices.com',
+        'A1805IZSGTT6HS' => 'mws-eu.amazonservices.com', // NL
+        'A2NODRKZP88ZB9' => 'mws-eu.amazonservices.com', // Sweden
+        'A1C3SOZRARQ6R3' => 'mws-eu.amazonservices.com', // PL
+        'A33AVAJ2PDY3EV' => 'mws-eu.amazonservices.com', // Turkey
+        'A17E79C6D8DWNP' => 'mws-eu.amazonservices.com', // Saudi Arabia
+        'ARBP9OOSHTCHU' => 'mws-eu.amazonservices.com', // Egypt
+        'A2VIGQ35RCS4UG' => 'mws.amazonservices.ae', // United Arab Emirates
+        // Far East Region
         'A1VC38T7YXB528' => 'mws.amazonservices.jp', // JP
         'A1VN0HAN483KP2' => 'mws.amazonservices.jp', // JP
-        'AAHKV2X7AFYLW' => 'mws.amazonservices.com.cn', //CN
+        'A19VAU5U5O7RUS' => 'mws-fe.amazonservices.com', // SG
         'A39IBJ37TRP1C6' => 'mws.amazonservices.com.au', // AU
-        'A2Q3Y263D00KWC' => 'mws.amazonservices.com', // BR
-        'A1805IZSGTT6HS' => 'mws-eu.amazonservices.com', // NL
-        'A2NODRKZP88ZB9' => 'mws-eu.amazonservices.com', // SE
-        'A1C3SOZRARQ6R3' => 'mws-eu.amazonservices.com', // PL
-        'A33AVAJ2PDY3EV' => 'mws-eu.amazonservices.com', // TR
     ];
 }
